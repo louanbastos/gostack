ae12a9081c0d7cc75de7d0b8a7d41b3b58c25664
Louan Fontenele <louanbastos61@gmail.com>
2020-11-03 10:16:00 -0300

    TASK-3
    TASK-3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 const express = require("express");
 
 const app = express();
-var date = new Date().toLocaleDateString();
-
-/* you can use req (request) or res (response) */
-app.get("/", (req, res) => {
-  res.status(200);
-  return res.json([
-    {
-      code: "200",
-      stats: "Success",
-      date: `${date}`,
-    },
-  ]);
-});
 
 app.get("/projects", (req, res) => {
   return res.json(["Project01", "Project02"]);
@@ -23,9 +10,15 @@ app.post("/projects", (request, response) => {
   return res.json(["Project01", "Project02", "Project03"]);
 });
 
+app.put("/projects/:id", (request, response) => {
+  return res.json(["Project04", "Project02", "Project03"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return res.json(["Project02", "Project03"]);
+});
+
 app.listen(3333, () => {
   console.clear();
   console.log("🚀 Backend Started");
 });
-/*  */
-// app.listen(80);
